099f15784c2ff75bdafa7d910ced339b3ad5c2f5
xMaNaSx <72237817+xMaNaSx@users.noreply.github.com>
2021-12-06 11:31:06 +0530

    Update Insert a node at a specific position in a linked list.cpp

--- LinkedList/Insert a node at a specific position in a linked list.cpp
+++ LinkedList/Insert a node at a specific position in a linked list.cpp
@@ -57,41 +57,43 @@ void free_singly_linked_list(SinglyLinkedListNode* node) {
     }
 }
 
-/*
- * Complete the 'insertNodeAtPosition' function below.
- *
- * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
- * The function accepts following parameters:
- *  1. INTEGER_SINGLY_LINKED_LIST llist
- *  2. INTEGER data
- *  3. INTEGER position
- */
 
-/*
- * For your reference:
- *
- * SinglyLinkedListNode {
- *     int data;
- *     SinglyLinkedListNode* next;
- * };
- *
- */
 
-SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
-int i=0;
-SinglyLinkedListNode* p=llist;
-SinglyLinkedListNode* ptr=new SinglyLinkedListNode(data);
-ptr->data=data;
-while(i<position){
-    p=p->next;
-    i++;
-    if(i==position-1)
-    {
-        ptr->next=p->next;
-        p->next=ptr;
-    }
-}
-return llist;
-}
 
 int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int data;
+    cin >> data;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist_head = insertNodeAtPosition(llist->head, data, position);
+
+    print_singly_linked_list(llist_head, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist_head);
+
+    fout.close();
+
+    return 0;
+}
