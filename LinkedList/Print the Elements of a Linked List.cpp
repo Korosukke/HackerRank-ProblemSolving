b911e290428a700bd72dcb69225e98b1f91b9b6a
xMaNaSx <72237817+xMaNaSx@users.noreply.github.com>
2021-12-06 11:30:01 +0530

    Update Print the Elements of a Linked List.cpp

--- LinkedList/Print the Elements of a Linked List.cpp
+++ LinkedList/Print the Elements of a Linked List.cpp
@@ -66,3 +66,22 @@ while(head!=NULL)
 }
 
 int main()
+{
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    printLinkedList(llist->head);
+
+    return 0;
+}
